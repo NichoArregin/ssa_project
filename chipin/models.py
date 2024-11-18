8114ec346a5313660f1960856eb3611a70e1e82b
NichoArregin <narregin@parrastu.catholic.edu.au>
2024-11-19 09:37:10 +1100

    failed code week 4

--- chipin/models.py
+++ chipin/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Group(models.Model):
     name = models.CharField(max_length=100)
     admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin_groups')
